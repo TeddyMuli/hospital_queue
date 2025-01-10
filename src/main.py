36199c9e3e7c9ffda4c73b87e9c97af1a86e6dc0
TeddyMuli <tmuli974@gmail.com>
2025-01-10 05:56:33 +0300

    add assets

--- src/main.py
+++ src/main.py
@@ -2,9 +2,14 @@
 import tkinter as tk
 from hospital_queue import HospitalQueue
 
+image_path = "../assets/person.png"
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title('Hospital Queue')
     root.geometry('600x500')
 
+    image = tk.PhotoImage(file=image_path)
+    label = tk.Label(root, image=image)
+    label.pack()
     root.mainloop()
