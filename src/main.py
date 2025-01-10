ecf61708dc39cb7a1e25e909e670cb9646ef3ab5
TeddyMuli <tmuli974@gmail.com>
2025-01-10 05:33:49 +0300

    init tkinter

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
-from hospital_queue import HospitalQueue
+#!/usr/bin/env python3
 import tkinter as tk
+from hospital_queue import HospitalQueue
 
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title('Hospital Queue')
+    root.geometry('600x500')
 
+    root.mainloop()
